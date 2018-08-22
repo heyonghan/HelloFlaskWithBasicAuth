a26877fe94702daccca6f907107d737a195ea9fa
heyonghan <dlhyhhe@cn.ibm.com>
2018-08-22 15:52:00 +0800

    Initial commit

---
+++ HelloFlask.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+app = Flask(__name__)
+
+users = [
+    {'username': 'Tom', 'password': '111111'},
+    {'username': 'Michael', 'password': '123456'},
+    {'username': 'admin', 'password': '333333'}
+]
+
+
+@auth.get_password
+def get_password(username):
+    for user in users:
+        if user['username'] == username:
+            return user['password']
+    return None
+
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hello, %s!" % auth.username()
+
+if __name__ == '__main__':
+    app.run()
